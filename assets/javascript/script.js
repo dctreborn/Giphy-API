041b7c87c6b66b74cf0099094ad5a58cbbb2df52
Rukaroa <rukaroa@gmail.com>
2017-02-26 20:54:09 -0600

    html skeleton

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+var topics = []; // holds searches
+//10 static images per search
+//images should animate on click
+//display image rating
+//http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
+/*
+
+    q - search query term or phrase
+    limit - (optional) number of results to return, maximum 100. Default 25.
+    offset - (optional) results offset, defaults to 0.
+    rating - (optional) limit results to those rated (y,g, pg, pg-13 or r).
+    lang - (optional) specify default country for regional content; format is 2-letter ISO 639-1 country code. See list of supported languages here
+    fmt - (optional) return results in html or json format (useful for viewing responses as GIFs to debug/test)
+/*
