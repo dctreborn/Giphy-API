45624ee6ca160c23bd7f6fdbdb7a4a719d08e8e3
Rukaroa <rukaroa@gmail.com>
2017-02-27 18:57:18 -0600

    posted images from api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,35 +4,25 @@ topics = ["rabbit","shoebill","lion"];
 //10 static images per search
 //images should animate on click
 //display image rating
-//http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
-/*
-
-    q - search query term or phrase
-    limit - (optional) number of results to return, maximum 100. Default 25.
-    offset - (optional) results offset, defaults to 0.
-    rating - (optional) limit results to those rated (y,g, pg, pg-13 or r).
-    lang - (optional) specify default country for regional content; format is 2-letter ISO 639-1 country code. See list of supported languages here
-    fmt - (optional) return results in html or json format (useful for viewing responses as GIFs to debug/test)
-*/
 
 //make button
 $("#add").on("click", function(event) {
 	event.preventDefault();
-	
-	console.log(event.preventDefault());
 
 	var entry = $("#tag-input").val().trim();
-	var btn = $("<button>");
-
-	topics.push(entry);
-	btn.attr("data-search",entry);
-	btn.attr("id","search");
-	btn.text(entry);
-	$("#buttons").append(btn);
-})
+	if (entry == ""){} //do nothing
+	else {
+		var btn = $("<button>");
+		topics.push(entry);
+		btn.attr("data-search",entry);
+		btn.addClass("search");
+		btn.text(entry);
+		$("#buttons").append(btn);
+	}
+});
 
 //search button item
-$("#search").on("click", function(){
+$(document.body).on("click", ".search", function(){
 	console.log("searching...");
 	var target = $(this).attr("data-search").replace(/\s/,"+");
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + target + "&limit=10&api_key=dc6zaTOxFJmzC";
@@ -42,20 +32,27 @@ $("#search").on("click", function(){
 		method: "GET"
 	}).done(function(response){
 		console.log(response);
+		response = response.data;
+		var length = response.length;
+		console.log(length);
 
-		var div = $("<div>");
-		var img = $("<img>");
-		var p = $("<p>");
-		p.text("Rating: " + data.rating.toUpperCase());
-		img.attr("src", data.images.original_still.url);
-		img.attr("state","still");
-		img.attr("data-still", data.images.original_still.url);
-		img.attr("data-animated", data.images.original.url);
-		div.append(p);
-		div.append(img);
+		for (var i = 0; i < length; i++) {
+			console.log("loop " + i);
+			var div = $("<div>");
+			var img = $("<img>");
+			var p = $("<p>");
+			p.text("Rating: " + response[i].rating.toUpperCase());
+			img.attr("src", response[i].images.original_still.url);
+			img.attr("state","still");
+			img.attr("data-still", response[i].images.original_still.url);
+			img.attr("data-animated", response[i].images.original.url);
+			div.append(p);
+			div.append(img);
+			$("#images").append(div);
+		}
 
 
 	}).fail(function(err) {
 	  throw err;
 	});
-})
+});
